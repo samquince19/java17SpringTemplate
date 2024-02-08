ec61bd23559500e429026decdcd55b3a3ec3a710
Sam Quince <samthedev19@gmail.com>
2024-02-07 20:28:06 -0700

    initial commit

---
+++ src/main/java/com/example/KrogerApi4/KrogerApi4Application.java
@@ -0,0 +1,13 @@
+package com.example.KrogerApi4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrogerApi4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KrogerApi4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/KrogerApi4/KrogerApi4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.KrogerApi4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrogerApi4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
